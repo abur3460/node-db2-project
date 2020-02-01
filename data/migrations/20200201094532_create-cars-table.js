621239b22db8426d52c3da1c75d540ec8d587442
Austin Burton <abur3460@gmail.com>
2020-02-01 10:24:40 -0800

    still working, slight changes to table

--- data/migrations/20200201094532_create-cars-table.js
+++ data/migrations/20200201094532_create-cars-table.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+      tbl.integer('VIN')
+        .unique()
+        .notNullable();
+      tbl.text('Make')
+        .notNullable();
+      tbl.text('Model')
+        .notNullable(); 
+      tbl.integer('Milage')
+        .notNullable();  
+      tbl.text('Transmission');
+      tbl.text('Title')  
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
